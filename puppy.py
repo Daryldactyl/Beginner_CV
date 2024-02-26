7a8abb30be6dd2768d88db7907db295c5c55c5be
Daryl Roberts <144744680+Daryldactyl@users.noreply.github.com>
2024-02-26 14:47:45 -0600

    Add files via upload

---
+++ puppy.py
@@ -0,0 +1,15 @@
+import cv2
+
+img = cv2.imread('../DATA/00-puppy.jpg')
+
+# reimg=cv2.resize(img,(0,0),img,0.5,0.5)
+
+while True:
+
+    cv2.imshow('Puppy',img)
+
+    if cv2.waitKey(1) & 0xff == 27:
+
+        break
+
+cv2.destroyAllWindows()
